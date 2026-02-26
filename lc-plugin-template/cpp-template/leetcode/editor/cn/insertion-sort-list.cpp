4d917eb22583c4648718727a225d850f27c5d09e
dizzyzzh <dizzyzzh@gmail.com>
2026-02-26 19:45:24 +0800

    add labuladong cpp template

---
+++ lc-plugin-template/cpp-template/leetcode/editor/cn/insertion-sort-list.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=147 lang=cpp
+ * @lcpr version=
+ *
+ * [147] 对链表进行插入排序
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../common/ListNode.cpp"
+#include "../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}
+
+
+
+/*
+// @lcpr case=start
+// [4,2,1,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-1,5,3,4,0]\n
+// @lcpr case=end
+
+ */
+
